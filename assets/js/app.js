8f6935ea5e9168612b165005551e2b1c1170f398
Franco Contreras~ <24.f.contreras@gmail.com>
2023-04-18 21:20:00 -0400

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+console.log("🟢 Connected!");
+
+const formulario = document.querySelector("#formulario");
+const terminos = document.querySelector("#terminos");
+const correo = document.querySelector("#correo");
+
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (!terminos.checked || !correo.value) {
+    alert("Parece que nos faltó algo...");
+  } else {
+    alert("¡Felicitaciones! Pronto escucharás de nosotros");
+    formulario.reset();
+  }
+});
